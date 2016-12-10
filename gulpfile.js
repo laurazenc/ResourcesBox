7a05c3a6184f1d8dde78a8a04452cba3886500ba
Laura Coalla Cencerrado <laurazenc@gmail.com>
2016-12-10 18:29:08 +0100

    Set up basic project structure and dependencies

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var nodemon = require('gulp-nodemon');
+var browserSync = require('browser-sync');
+var path = require('path');
+var exec = require('child_process').exec;
+
+
+var BROWSER_SYNC_RELOAD_DELAY = 500;
+var reload = browserSync.reload();
+
+gulp.task('sass', function(){
+	gulp.src('app/assets/style.scss')
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(gulp.dest('app/assets'));
+});
+
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('watch', function(){
+	gulp.watch('app/assets/**/*.scss', ['sass', 'bs-reload']);
+	gulp.watch('app/**/*.html', ['bs-reload']);
+	gulp.watch('app/**/*.js', ['bs-reload']);
+	gulp.watch('app/app.js', ['bs-reload']);
+  gulp.watch('app/index.html', ['bs-reload']);
+});
+
+gulp.task('styles', function(){
+  return gulp.src('app/assets/**/*.scss')
+  .pipe(sass({ style: 'expanded', loadPath: [ path.join( __dirname , 'app/assets') ] }))
+  .pipe(gulp.dest('app/assets'))
+});
+
+gulp.task('serve', function (cb) {
+	var called = false;
+  return nodemon({
+
+    // nodemon our expressjs server
+    script: 'server.js',
+
+    // watch core server file(s) that require server restart on change
+    watch: ['server.js', 'server/**/*.js']
+  })
+    .on('start', function onStart() {
+      // ensure start only got called once
+      if (!called) { cb(); }
+      called = true;
+    })
+    .on('restart', function onRestart() {
+      // reload connected browsers after a slight delay
+      setTimeout(function reload() {
+        browserSync.reload({
+          stream: false
+        });
+      }, BROWSER_SYNC_RELOAD_DELAY);
+    });
+});
+
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		notify: false,
+		proxy: 'http://localhost:5000',
+    port: 5000,
+		browser: ['google chrome']
+  });
+});
+
+
+
+gulp.task('default', ['run', 'browser-sync', 'sass', 'watch', 'styles']);
+
+gulp.task('run', function (cb) {
+  exec('firebase serve', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
